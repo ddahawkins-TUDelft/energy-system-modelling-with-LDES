44589e5cd13b4b15758847c3d5538c728edeef30
thedanodewi <danodewi@gmail.com>
2025-01-31 20:29:09 +0100

    Performed single year runs, adding these results file to the main repository

--- weather_year_clustering_LDES_study/python/batch_reference_run.py
+++ weather_year_clustering_LDES_study/python/batch_reference_run.py
@@ -9,9 +9,6 @@ model_type = "reference" # model type
 results_directory = f"weather_year_clustering_LDES_study/results/{model_type}" #save directory for results
 calliope.set_log_verbosity("INFO", include_solver_output=True) #if True, shows calliope logs as output
 
-
-
-
 save_path = results_directory+'/results_2010_to_2019.netcdf'
 
 model = calliope.Model(
@@ -24,8 +21,6 @@ model.build()
 model.solve()
 model.to_netcdf(save_path)
 
-
-
 rv.chart_bar_flow_cap(model)
-print('breakpoint')
+print('breakpoint, in case something goes wrong with save that I can retain the variables to manually save... dont want to lose something after a 17hr run')
 print('breakpoint')
--- weather_year_clustering_LDES_study/python/batch_single_year_runs.py
+++ weather_year_clustering_LDES_study/python/batch_single_year_runs.py
@@ -1,16 +1,12 @@
 import calliope
-import datetime
 import pandas as pd
 import plotly.express as px
-import yaml
-import re
-import os
 import script_utilities as util #custom module to abstract away some frequently used functions
 
 # script configuration
 model_type = "single_year_cluster" # model type
 results_directory = f"weather_year_clustering_LDES_study/results/{model_type}" #save directory for results
-calliope.set_log_verbosity("INFO", include_solver_output=True) #if True, shows calliope logs as output
+# calliope.set_log_verbosity("INFO", include_solver_output=True) #if True, shows calliope logs as output
 
 
 # import weather years
@@ -19,7 +15,8 @@ df_casestudy = (util.read_years_weights(model_type)) #df of years and weights, o
 #loop over the case study years, generate and solve models for each, saving the results as netcdf
 for index, values in df_casestudy.iterrows():
 
-    save_path = results_directory+f'/results_years_[{values['years']}]_weight_[{values['weights']}].netcdf'
+    print('Running: ', index, values['years'])
+    save_path = results_directory+f'/results_year_{values['years']}.netcdf'
 
     model = calliope.Model(
         'weather_year_clustering_LDES_study/model_config/model.yaml',

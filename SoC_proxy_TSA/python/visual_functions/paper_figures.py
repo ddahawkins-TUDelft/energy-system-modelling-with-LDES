c3c31146e47369561724994b8e857f9237bf8784
thedanodewi <danodewi@gmail.com>
2025-11-19 11:57:55 +0100

    cleaned log data

--- SoC_proxy_TSA/python/visual_functions/paper_figures.py
+++ SoC_proxy_TSA/python/visual_functions/paper_figures.py
@@ -58,8 +58,8 @@ import matplotlib as mpl
 
 # Adjust these if your repo layout differs
 # Per your note: separate logs for figs 1–3 and 4–5:
-LOGS_F123     = [Path("SoC_proxy_TSA/data/notes/log_10_yr_WandKTests.csv")]
-LOGS_F45      = [Path("SoC_proxy_TSA/data/notes/log_horizon.csv")]
+LOGS_F123     = [Path("SoC_proxy_TSA/data/notes/log_14-180.csv")] 
+LOGS_F45      = [Path("SoC_proxy_TSA/data/notes/log_45-60-90.csv")]
 
 MODELS_DIR      = Path("SoC_proxy_TSA/data/calliope_models")
 PARAM_DIR       = Path("SoC_proxy_TSA/data/parameters")

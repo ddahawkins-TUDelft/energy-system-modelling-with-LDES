e2e4d65b5b82a013c65475be4af91c4da75dfd01
thedanodewi <danodewi@gmail.com>
2025-11-28 15:35:02 +0100

    BE runs first two year sets

--- SoC_proxy_TSA/python/post_optimised_cluster_tsa_dispatcher.py
+++ SoC_proxy_TSA/python/post_optimised_cluster_tsa_dispatcher.py
@@ -11,7 +11,7 @@ import time
 # ----------------------------------- CONFIGURE ----------------------------------------------
 
 show_soc = True
-show_visual = True
+show_visual = False
 
 country = 'BE'
 
@@ -56,8 +56,8 @@ dispatch_config = [
     # ['SoC_proxy_TSA/data/timeseries/time_varying_parameters.csv', [2010,2019]],
     # ['SoC_proxy_TSA/data/timeseries/time_varying_parameters.csv', [2012,2021]],
     # ['SoC_proxy_TSA/data/timeseries/time_varying_parameters.csv', [2014,2023]],
-    [f'SoC_proxy_TSA/data/timeseries/time_varying_parameters_{country}.csv', [2006,2015]],
-    # [f'SoC_proxy_TSA/data/timeseries/time_varying_parameters_{country}.csv', [2008,2017]],
+    # [f'SoC_proxy_TSA/data/timeseries/time_varying_parameters_{country}.csv', [2006,2015]],
+    [f'SoC_proxy_TSA/data/timeseries/time_varying_parameters_{country}.csv', [2008,2017]],
     # [f'SoC_proxy_TSA/data/timeseries/time_varying_parameters_{country}.csv', [2010,2019]],
     # [f'SoC_proxy_TSA/data/timeseries/time_varying_parameters_{country}.csv', [2012,2021]],
     # [f'SoC_proxy_TSA/data/timeseries/time_varying_parameters_{country}.csv', [2014,2023]],
